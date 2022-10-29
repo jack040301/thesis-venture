a77d4e81b53ad177d78aacbe920eb8a97c93d4c1
Jacqueline Porral <jackdaisuki04@gmail.com>
2022-10-30 07:32:24 +0800

    Title app changed

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Flutter Log In & Sign up Authentication with FireBase",
+      title: "Venture",
       home: AuthScreen(),
     );
   }
