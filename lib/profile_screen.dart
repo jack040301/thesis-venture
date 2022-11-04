a82f978f52666c1e930b197d954e64e6cd323b83
demverleenespinola <espinola.demverleen.bscs2019@gmail.com>
2022-11-05 01:14:32 +0800

    handling null value

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -20,6 +20,7 @@ Future<void> logOut() async {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,26 +28,15 @@ class _ProfileScreenState extends State<ProfileScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.network(FirebaseAuth.instance.currentUser!.photoURL!),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              FirebaseAuth.instance.currentUser!.displayName!,
-              style: const TextStyle(
-                  fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black87),
+            Image.network(FirebaseAuth.instance.currentUser!.photoURL??""),
+            SizedBox(height: 20,),
+            Text(FirebaseAuth.instance.currentUser!.displayName??"",
+              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black87),
             ),
             const SizedBox(
               height: 10,
             ),
-            Text(
-              FirebaseAuth.instance.currentUser!.email!,
-              style: const TextStyle(
-                  fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black87),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
+
             ElevatedButton(
                 onPressed: logOut, child: Text("Logout Account")
             ),
@@ -57,3 +47,4 @@ class _ProfileScreenState extends State<ProfileScreen> {
     );
   }
 }
+
