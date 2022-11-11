c9f3c27ba48fe00bed5125831255f9b32ec2017b
Jacqueline Porral <jackdaisuki04@gmail.com>
2022-11-11 20:15:46 +0800

    sign up merge

--- lib/auth_screen.dart
+++ lib/auth_screen.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:main_venture/auth_screens/login.dart';
-import 'package:main_venture/auth_screens/signup.dart';
 
 import 'package:main_venture/profile_screen.dart';
 
