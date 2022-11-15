9c51cab4e29df5cf035e9381f461e84c704901e6
demverleenespinola <espinola.demverleen.bscs2019@gmail.com>
2022-11-15 10:27:52 +0800

    changes in profile screen

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -28,7 +28,116 @@ Future<void> logOut() async {
   }
 }
 
+
+
 class _ProfileScreenState extends State<ProfileScreen> {
+
+  // List<ListItem> _dropdownItems = [
+  //   ListItem(1, "GeeksforGeeks"),
+  //   ListItem(2, "Javatpoint"),
+  //   ListItem(3, "tutorialandexample"),
+  //   ListItem(4, "guru99")
+  // ];
+  //
+  // List<DropdownMenuItem<ListItem>> _dropdownMenuItems;
+  // ListItem _itemSelected;
+  //
+  // void initState() {
+  //   super.initState();
+  //   _dropdownMenuItems = buildDropDownMenuItems(_dropdownItems);
+  //   _itemSelected = _dropdownMenuItems[1].value;
+  // }
+  //
+  // List<DropdownMenuItem<ListItem>> buildDropDownMenuItems(List listItems) {
+  //   List<DropdownMenuItem<ListItem>> items = List();
+  //   for (ListItem listItem in listItems) {
+  //     items.add(
+  //       DropdownMenuItem(
+  //         child: Text(listItem.name),
+  //         value: listItem,
+  //       ),
+  //     );
+  //   }
+  //   return items;
+  // }
+
+  createAlertDialog(BuildContext context){
+    TextEditingController customController  = TextEditingController();
+
+    return showDialog(context: context, builder: (context){
+      return AlertDialog(
+
+        title: Container(
+
+          child: Column(
+            children: [
+              const Text('What kind of business do you prefer?'),
+              // DropdownButtonHideUnderline(
+              //   child: DropdownButton(
+              //       // value: _itemSelected,
+              //       // items: _dropdownMenuItems,
+              //       // onChanged: (value) {
+              //       //   setState(() {
+              //       //     _itemSelected = value;
+              //       //   });
+              //       // }),
+              // ),
+
+
+              TextField(
+                controller: customController,
+              decoration: InputDecoration(
+                  //labelText: 'What kind of business do you prefer?',
+                  border: OutlineInputBorder(),
+                  hintText: 'Choose your business'),
+              ),
+              const Text('Enter budget for the area (per square meter)'),
+              TextField(
+                controller: customController,
+                decoration: InputDecoration(
+                  //labelText: 'Enter budget for the area (per square meter)',
+                  border: OutlineInputBorder(),
+                  //hintText: 'Choose your business'),
+                ),
+              ),
+              const Text('Enter value of area (square meter)'),
+              TextField(
+                controller: customController,
+                decoration: InputDecoration(
+                  //labelText: 'Enter value of area (square meter)',
+                  border: OutlineInputBorder(),
+                  //hintText: 'Choose your business'),
+                ),
+              ),
+            ],
+
+          ),
+        ),
+
+        // title: Text(""),
+        // content: TextField(
+        //   controller: customController,
+        //     decoration: InputDecoration(
+        //       border: OutlineInputBorder(),
+        //       hintText: 'Enter a search term',),
+        //
+        // ),
+
+
+        actions: <Widget>[
+          MaterialButton(
+            elevation: 5.0,
+            child: Text('OK'),
+            onPressed: (){
+              Navigator.of(context).pop(customController.text.toString());
+            },
+          )
+        ],
+
+
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,29 +146,50 @@ class _ProfileScreenState extends State<ProfileScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            FirebaseAuth.instance.currentUser!.photoURL == null ? const Image(image: AssetImage('assets/images/pic.png')) : Image.network(FirebaseAuth.instance.currentUser!.photoURL??""),
+            FirebaseAuth.instance.currentUser!.photoURL == null
+                ? const Image(image: AssetImage('assets/images/pic.png'))
+                : Image.network(FirebaseAuth.instance.currentUser!.photoURL??""),
             Text(
               FirebaseAuth.instance.currentUser!.displayName ?? "Default Name",
-              style: const TextStyle(
-                fontSize: 30,),
+              style: const TextStyle(fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87),
             ),
             const SizedBox(
               height: 10,
             ),
-            ElevatedButton(onPressed: logOut, child: Text("Logout Account")),
+            // Text(
+            //   FirebaseAuth.instance.currentUser!.uid,
+            //   style: const TextStyle(fontSize: 30,
+            //       fontWeight: FontWeight.bold,
+            //       color: Colors.black87),
+            // ),
+            // const SizedBox(
+            //   height: 10,
+            // ),
+
+            ElevatedButton(
+                onPressed: logOut, child: Text("Logout Account")
+            ),
             ElevatedButton(
                 onPressed: () {
                   Navigator.push(context,
                       MaterialPageRoute(builder: (context) => HomePage()));
                 },
-                child: Text("Map")),
+                child: Text("Map")
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  createAlertDialog(context);
+                },
+                child: Text("Dialog")
+            ),
           ],
         ),
       ),
+
     );
   }
-
-
 
 
 }
