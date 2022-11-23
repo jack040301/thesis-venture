a6f376ebf359023f1f13304f9d8a93d7a54604dc
Jacqueline Porral <jackdaisuki04@gmail.com>
2022-11-23 09:28:38 +0800

    recent update fix

--- lib/auth_screen.dart
+++ lib/auth_screen.dart
@@ -1,4 +1,4 @@
-/*import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:main_venture/auth_screens/login.dart';
 
--- lib/main.dart
+++ lib/main.dart
@@ -3,34 +3,10 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:main_venture/dialogbox.dart';
 import 'package:main_venture/auth_screen.dart';
 
-
-/* import 'package:main_venture/auth_screens/signup.dart';
-import 'package:main_venture/navigation.dart';
-
-
-/*import 'package:main_venture/navigation.dart';
-
-import 'package:main_venture/dialog.dart';
-import 'package:main_venture/dialogbutton.dart';*/
-
-import 'package:main_venture/settings.dart';
-
-/*import 'package:main_venture/navigation.dart';*/
-import 'package:main_venture/dialogbutton.dart';
-
-/*import 'package:main_venture/navigation.dart';
-import 'package:main_venture/dialog.dart';*/
-
-import 'package:main_venture/dialogbutton.dart';
-
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:lottie/lottie.dart';
 import 'package:main_venture/pinned_location.dart';
-
 
 void main() async {
   /// initialize FireBase App
@@ -63,16 +39,13 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
-
+  @override
+  void initState() {
+    super.initState();
     Future.delayed(const Duration(seconds: 5)).then((value) =>
         Navigator.of(context).pushReplacement(
-
-
             MaterialPageRoute(builder: (context) => const AuthScreen())));
-
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -80,8 +53,7 @@ class _SplashScreenState extends State<SplashScreen> {
       splash: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image.asset('assets/animassets/Logo.gif', width: 250,
-              height: 150)
+          Image.asset('assets/animassets/Logo.gif', width: 250, height: 150)
         ],
       ),
       nextScreen: const PinnedLocation(),
