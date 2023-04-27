f0d07ab99b31b3b60bec2e96f0a18962e3fc6b7c
jack daisuki <jackdaisuki04@gmail.com>
2023-04-28 07:51:33 +0800

    manage request v.1 unstable and untested

--- src/pages/ManageRequest.js
+++ src/pages/ManageRequest.js
@@ -1,11 +1,23 @@
 import React, {useEffect, useState} from "react";
-import {collection, db,onSnapshot, query,where, limit,orderBy } from "../firebase";
+import {collection, db,onSnapshot, query,where, limit,orderBy, doc, updateDoc,getDoc } from "../firebase";
+import {
+  MDBModal,
+  MDBModalDialog,
+  MDBModalContent,
+  MDBModalHeader,
+  MDBModalTitle,
+  MDBModalBody,
 
+  MDBBtn,
+} from "mdb-react-ui-kit";
 
 function ManageRequest() {
 
   const [data, setData] = useState([]);
    const [search,setSearch] = useState([]);
+   const [status_modal, setStatusModal] = useState(false);
+   const [userStat, setUserStat] = useState("");
+
 
 
   useEffect(()=>{
@@ -17,11 +29,19 @@ function ManageRequest() {
   function fulldata () {
 
   
-    const collect = query(collection(db,"users"),where("role","==","user"));
+    const collect = query(collection(db,"markers"),where("request_status","==",false), where("user_id_requested","==","NLz3guI5pEOXKJEyDzz1tsdcEtF2"));
     const unsub = onSnapshot(collect, snapshot =>{
 
-      const admintable = snapshot.docs.map(doc=> ({ email:doc.data().email, adminid:doc.id}
+
+      
+
+      const admintable = snapshot.docs.map(doc=> ({ place:doc.data().place, adminid:doc.id, data: doc.data().createdAt,
+        user_id_requested : doc.data().user_id_requested,
+    }
       ))
+
+      
+   
 
 
      setData(admintable)
@@ -38,7 +58,7 @@ function ManageRequest() {
 
   if(search !== null && search !== ""){
 
-const collect = query(collection(db,"users"),where("role","==","user"), where("email",">=",search), orderBy("email"), limit(1));
+const collect = query(collection(db,"markers"),where("request_status","==",false), where("user_id_requested",">=",search), orderBy("user_id_requested"), limit(2));
     const unsub = onSnapshot(collect, snapshot =>{
 
       const admintable = snapshot.docs.map(doc=> ({ email:doc.data().email, adminid:doc.id}
@@ -60,8 +80,43 @@ const collect = query(collection(db,"users"),where("role","==","user"), where("e
     
   }
 
+
   
   } 
+
+
+  const setApproveRequest = async (e) => {
+    const docRef = doc(db, "markers", e);
+    const docSnap = await getDoc(docRef);
+
+    const updateStatus = await updateDoc(docRef, {
+        
+request_status: true,
+    });
+
+    alert("Successfull");
+    
+
+ /*    if(docSnap.data().status == "Active"){
+      const updateStatus = await updateDoc(docRef, {
+        
+        status: "Deactivated",
+        btn: "Reactivate",
+      });
+
+      //setStatus_s("status-deactivated");
+      console.log(docSnap.data().status);
+    }else{
+      const updateStatus = await updateDoc(docRef, {
+        status: "Active",
+        btn: "Deactivate",
+      });
+      console.log(docSnap.data().status); */
+      //setStatus_s("status-active");
+      
+    
+  };
+
 
 
 
@@ -104,22 +159,30 @@ const collect = query(collection(db,"users"),where("role","==","user"), where("e
                       <th>Timestamp</th>
                       <th>User Request</th>
                       <th>Status</th>
+                      <th>Action</th>
+
                     </tr>
                   </thead>
                   <tbody id="tbody1">
 
-                  { 
-              data.map( (mark) => (
+        { 
+              data.map( (markers_request) => (
                    
-                
-                <tr key={mark.adminid}>
-                <td>{mark.adminid}</td>
-                <td>{mark.email}</td>
-                <td>Users</td>
-                <td>Daily</td>
+             
+
+                <tr key={markers_request.adminid}>
+                <td>{markers_request.adminid}</td>
+
+                <td>{markers_request.place}</td>
+                <td>{markers_request.data.seconds}, {markers_request.data.nanoseconds}</td>
+                <td>{markers_request.user_id_requested}</td>
+
 
                 <td>
                   <span className="tag tag-success">Approved</span>
+                </td>
+                <td>                 
+                  <button onClick={(e)=>{setStatusModal(true); setUserStat(e.currentTarget.id)}} id={markers_request.adminid} class="statBtn">Approve Request</button>                  
                 </td>
                 
               </tr>
@@ -129,6 +192,19 @@ const collect = query(collection(db,"users"),where("role","==","user"), where("e
                   </tbody>
                 </table>
               </div>
+              <MDBModal show={status_modal} tabIndex='-1' setShow={setStatusModal}>
+              <MDBModalDialog size='sm'>
+                <MDBModalContent>
+                  <MDBModalHeader>
+                    <MDBModalTitle>Sure?</MDBModalTitle>                    
+                  </MDBModalHeader>
+                  <MDBModalBody>
+                    <MDBBtn className='btn-ok' color='none' onClick={()=>{setApproveRequest(userStat); setStatusModal(!status_modal);}}>Yes</MDBBtn>
+                    <MDBBtn className='btn-close' color='none' onClick={()=>{setStatusModal(!status_modal)}}>No</MDBBtn>
+                  </MDBModalBody>
+                </MDBModalContent>
+              </MDBModalDialog>
+            </MDBModal>
             </div>
           </div>
         </div>
